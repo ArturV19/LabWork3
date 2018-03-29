bad37950e4280c2e520f1718ff1fca86975defcd
ArturV19 <ArturVoekov@mail.ru>
2018-03-29 12:14:49 +0400

    Первая рабочая версия программы.

--- src/menu/WorkWithMatrix.java
+++ src/menu/WorkWithMatrix.java
@@ -1,5 +1,7 @@
 package menu;
 
+import exeptions.DimensionsOfMatricesAreNotEqualExeption;
+import exeptions.MatricesCanNotBeMultiplied;
 import matrix.RealMatrix;
 import matrix.WriteMatrix;
 
@@ -12,6 +14,11 @@ public class WorkWithMatrix {
 
     public static void run() {
         String userChoice;
+        System.out.println("'+' - Сложение");
+        System.out.println("'-' - Вычитание");
+        System.out.println("'*' - Произведение");
+        System.out.println("'A' - Анализ");
+        System.out.println("'T' - Транспонирование");
 
         while (true) {
             System.out.println("\n Начало ввода:");
@@ -42,5 +49,54 @@ public class WorkWithMatrix {
      */
     public static void mainJobWithMatrix(int heightOfFirstMatrix) {
         RealMatrix matrix = new RealMatrix(WriteMatrix.writeMatrix(heightOfFirstMatrix));
+
+        System.out.println("Введите команду: ");
+        String userChoiceOperation = MainMenu.scanner.next();
+
+        switch (userChoiceOperation){
+            case"+":{
+                RealMatrix matrix2 = new RealMatrix(WriteMatrix.writeMatrix());
+                try {
+                    System.out.println("\n Результат:");
+                    System.out.println(matrix.sumOfTwoMatrtix(matrix2).toString());
+                } catch (DimensionsOfMatricesAreNotEqualExeption dimensionsOfMatricesAreNotEqualExeption) {
+                    dimensionsOfMatricesAreNotEqualExeption.printStackTrace();
+                }
+                break;
+            }
+            case"-":{
+                RealMatrix matrix2 = new RealMatrix(WriteMatrix.writeMatrix());
+                try {
+                    System.out.println("\n Результат:");
+                    System.out.println(matrix.differenceOfTwoMatrtix(matrix2).toString());
+                } catch (DimensionsOfMatricesAreNotEqualExeption dimensionsOfMatricesAreNotEqualExeption) {
+                    dimensionsOfMatricesAreNotEqualExeption.printStackTrace();
+                }
+                break;
+            }
+            case"*":{
+                RealMatrix matrix2 = new RealMatrix(WriteMatrix.writeMatrix());
+                try {
+                    System.out.println("\n Результат:");
+                    System.out.println(matrix.multiplicationOfTwoMatrix(matrix2).toString());
+                } catch (MatricesCanNotBeMultiplied matricesCanNotBeMultiplied) {
+                    matricesCanNotBeMultiplied.printStackTrace();
+                }
+                break;
+            }
+            case "A":{
+                System.out.println("\n Результат:");
+                RealMatrix.analysisOfMatrix(matrix);
+                break;
+            }
+            case "T":{
+                System.out.println("\n Результат:");
+                System.out.println(RealMatrix.transposeOfMatrix(matrix).toString());
+                break;
+            }
+            default:{
+                System.out.println("Ошибка! Неизвестная команда.");
+            }
+        }
     }
 }
